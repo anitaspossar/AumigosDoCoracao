e2d36107dc458db11e177f5f62af7bbec29f1295
anitaspossar <anitaspossarrj@gmail.com>
2025-06-16 16:11:44 -0300

    Update pacientes.c
    Adicionado as funções de consulta do paciente, falta terminar somente duas.

--- pacientes.c
+++ pacientes.c
@@ -7,22 +7,19 @@
 p_paci pacientes[MAX_Pacientes];
 
 //funcao para remover o buffer caracteres restantes da memoria do teclado
-void limparBufferEntrada()
-{
+void limparBufferEntrada(){
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
 }
 
-void removerNovaLinha(char * str)
-{
+void removerNovaLinha(char * str){
     size_t len = strlen(str);
     if (len > 0 && str[len -1] == '\n') {
         str[len - 1] = '\0';
     }
 }
 
-void _exibirDadosPaciente(paciente *p_a)
-{
+void exibirDadosPaciente(paciente *p_a){
     if (p_a == NULL) {
         printf("Referencia de paciente nula... \n");
         return;
@@ -32,22 +29,20 @@ void _exibirDadosPaciente(paciente *p_a)
     printf("Nome Paciente: %s \n", p_a->NOMEp);
     printf("Idade do paciente: %d \n", p_a->IDADE);
     printf("Nome Tutor: %s \n", p_a->NOMEt);
-    printf("Raca: %s \n", p_a->RACA);
     printf("Especie: %s \n", p_a->ESPECIE);
+    printf("Raca: %s \n", p_a->RACA);
     printf("Telefone para contato: %d \n", p_a->TELEFONE);
     printf("-----------------------------\n");
 }
 
-void inicializaPacientes()
-{
+void inicializaPacientes(){
     for (int i=0; i<MAX_Pacientes; i++) {
         pacientes[i] = NULL;
     }
 }
 
 //cadastrar novo paciente
-void cadastrarPaciente()
-{
+void cadastrarPaciente(){
     int i = 0;
     int posicao = -1;
     for (i=0; i<MAX_Pacientes;i++) {
@@ -97,4 +92,76 @@ void cadastrarPaciente()
     //raca, especie telefone
 
     printf("Paciente [%s] cadastrado com sucesso..\n", pacientes[posicao]->NOMEp);
+}
+
+//consultar um paciente por ID
+void consultarPacienteID(){
+    int encontrado = 0;
+    int idBusca;
+    printf("Digite o ID do paciente procurado: \n");
+    scanf("%d", &idBusca);
+    limparBufferEntrada();
+
+    for (int i=0; i<MAX_Pacientes; i++) {
+        if (pacientes[i] != NULL) {
+            if (pacientes[i]->ID == idBusca) {
+                printf("Aluno encontrado \n");
+                exibirDadosPaciente(pacientes[i]);
+                encontrado = 1;
+                break;
+            }
+        }
+    }
+    if (!encontrado) {
+        printf("Paciente nao encontrado...\n");
+    }
+}
+
+void consultarPacienteNome() {
+    char nomeBusca[MAX_NOMEP];
+    int encontrado = 0;
+    limparBufferEntrada();
+
+    printf("Digite o nome(ou parte do nome) a ser encontrado: \n");
+        fgets(nomeBusca, sizeof(nomeBusca), stdin);
+    removerNovaLinha(nomeBusca);
+
+    if(strlen(nomeBusca)==0) {
+        printf("O nome inserido não foi encontrado, digite novamente. \n");
+        return;
+    }
+    for(int i=0; i<MAX_Pacientes; i++) {
+        if(pacientes[i] != NULL) {
+            if(strstr(pacientes[i]->NOMEp, nomeBusca) != NULL) {
+                exibirDadosPaciente(pacientes[i]);
+                encontrado++;
+            }
+        }
+    }
+
+    void consultarPacienteEspecie() {
+        char especieBusca[MAX_ESPECIE];
+        int achado = 0;
+        limparBufferEntrada();
+
+        printf("Digite para consultar os pacientes pela raça");
+            fgets(especieBusca, sizeof(especieBusca), stdin);
+        removerNovaLinha((especieBusca));
+
+        if(strlen(especieBusca) == 0) {
+            printf("Escreva corretamente a especie a ser procurada. \n");
+            return;
+        }
+        for(int i=0; i<MAX_Pacientes; i++) {
+            if(strstr(pacientes[i]->ESPECIE, especieBusca) != NULL) {
+                exibirDadosPaciente(pacientes[i]);
+                achado++;
+            }
+        }
+    }
+
+    void consultarPacienteRaca() {
+
+    }
+    void consultarPacienteTutor();
 }
